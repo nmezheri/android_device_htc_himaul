fdaa524bfd8f52e5b75849485eaae80b26b41f55
Julian Veit <claymore1298@gmail.com>
2018-05-15 18:35:10 +0200

    himaul: Update fingerprint

--- lineage.mk
+++ lineage.mk
@@ -11,6 +11,6 @@ PRODUCT_DEVICE := himaul
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="htc_himaulatt" \
     PRODUCT_NAME="himaulatt_na_gen_unlock" \
-    PRIVATE_BUILD_DESC="4.27.502.7 CL870623 release-keys"
+    PRIVATE_BUILD_DESC="4.30.617.12 CL894012 release-keys"
 
-BUILD_FINGERPRINT := "htc/himaulatt_na_gen_unlock/htc_himaulatt:7.0/NRD90M/870623.7:user/release-keys"
+BUILD_FINGERPRINT := "htc/himaulatt_na_gen_unlock/htc_himaulatt:7.0/NRD90M/894012.12:user/release-keys"
